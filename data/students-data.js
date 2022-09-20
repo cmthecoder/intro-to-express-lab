11d7c18349c9742ddbb4fb72b5c1085e13d7944b
Chris Mems <christopher.mems@.com>
2022-09-20 16:48:35 -0500

    add if student graduated

--- data/students-data.js
+++ data/students-data.js
@@ -1,7 +1,7 @@
 const students = [
-  {text: 'Feed llama', done: true, _id: 125223},
-  {text: 'Sleep under the stars', done: false, _id: 127904},
-  {text: 'Buy milk', done: false, _id: 139608},
+  {text: 'Heather Foster', graduated: true, _id: 125223},
+  {text: 'Thomas Blatt', graduated: false, _id: 127904},
+  {text: 'Juju Xan', graduated: false, _id: 139608},
 ]
 
 export { 
